432e8825bc58f08cbf46251ffe007fe71fc50c5a
benchan777 <benchan777@gmail.com>
2021-01-06 20:40:17 -0800

    add web scraper

--- app.py
+++ app.py
@@ -43,6 +43,43 @@ def add_restaurant():
 @app.route('/search_restaurants', methods=['GET', 'POST'])
 def search_restaurants():
     ''' Search for new restaurants to add to databse '''
+    if request.method == 'POST':
+        food_type = request.form.get('food_type')
+        location = request.form.get('location')
+
+        restaurant_type = food_type.replace(" ", "+")
+        restaurant_location = location.replace(" ", "+")
+        
+        r = requests.get(f"https://www.yelp.com/search?find_desc={restaurant_type}&find_loc={restaurant_location}%2C+CA")
+        soup = BeautifulSoup(r.content, features="html5lib")
+        restaurants_list = soup.find_all("div", {"class":"container__09f24__21w3G hoverable__09f24__2nTf3 margin-t3__09f24__5bM2Z margin-b3__09f24__1DQ9x padding-t3__09f24__-R_5x padding-r3__09f24__1pBFG padding-b3__09f24__1vW6j padding-l3__09f24__1yCJf border--top__09f24__1H_WE border--right__09f24__28idl border--bottom__09f24__2FjZW border--left__09f24__33iol border-color--default__09f24__R1nRO"})
+        print(len(restaurants_list))
+
+        for i in range(0, len(restaurants_list)):
+            l = {}
+
+            try:
+                l["name"] = restaurants_list[i].find("a", {"class":"link__09f24__1kwXV link-color--inherit__09f24__3PYlA link-size--inherit__09f24__2Uj95"}).string
+            except:
+                l["name"] = None
+
+            try:
+                l["price"] = restaurants_list[i].find("span", {"class":"text__09f24__2tZKC priceRange__09f24__2O6le text-color--black-extra-light__09f24__38DtK text-align--left__09f24__3Drs0 text-bullet--after__09f24__1MWoX"}).string
+            except:
+                l["price"] = None
+            
+            try:
+                l["rating"] = restaurants_list[i].find("div", {"class":"i-stars__09f24__1T6rz i-stars--regular-4__09f24__2YrSK border-color--default__09f24__R1nRO overflow--hidden__09f24__3u-sw"}).get('aria-label')
+            except:
+                l["rating"] = "No Rating"
+
+            db.restaurants.insert_one(l)
+            print(l)
+
+        return render_template('home.html')
+
+    else:
+        return render_template('search_restaurants.html')
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
